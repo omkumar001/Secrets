4fb620acb9a8268017c8567edca2fe894d38f1ef
OmThakur92 <omkumarbhu@gmail.com>
2021-08-31 20:29:03 +0530

    updated the app.js and package.json

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
-
+// const encrypt=require("mongoose-encryption");
+//const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 
 const app = express();
@@ -26,7 +28,7 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const  User=new mongoose.model("User",userSchema);
 
@@ -62,22 +64,31 @@ app.post("/", function (req, res) {
 
 
 app.post("/register", function (req, res) {
+
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+
+        const email=req.body.username;
+        const pass=hash;
+       // md5(req.body.password)
+    
+        const newUser=new User({
+           email:email,
+           password:pass,
+        });
+    
+        newUser.save(function(err){
+    
+           if(err)
+           console.log(err);
+           else
+           res.render("secrets");
+        });
+        
+    });
      
-    const email=req.body.username;
-    const pass=req.body.password;
-
-    const newUser=new User({
-       email:email,
-       password:pass,
-    });
-
-    newUser.save(function(err){
-
-       if(err)
-       console.log(err);
-       else
-       res.render("secrets");
-    });
+ 
     // res.redirect("/");
 
 });
@@ -86,19 +97,25 @@ app.post("/register", function (req, res) {
 app.post("/login", function (req, res) {
 
 const username=req.body.username;
-const pass=req.body.password;
+const pass=req.body.password
+//md5(req.body.password);
 
-User.findOne({email:username,password:pass},function(err,foundUser){
+User.findOne({email:username},function(err,foundUser){
      
     if(err)
     console.log(err);
     else{
         if(foundUser){
-            res.render("secrets");
+            bcrypt.compare(pass,foundUser.password, function(err, result) {
+                // result == true
+
+                if(result){
+                    res.render("secrets");
+                   }
+              });
            }
            else
-           {   
-               
+           {     
                res.render("login",{status:"false",message:"Invalid Login Credentials !!"});
                console.log("User Not Found");
            }
